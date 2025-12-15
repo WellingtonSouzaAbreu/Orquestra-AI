e9f1ca363ef67bcf2a1fcbf6da58441520fd8997
Wellington Souza Abreu <64331839+WellingtonSouzaAbreu@users.noreply.github.com>
2025-12-15 14:44:37 -0400

    Refactor organization handling in InicioPage component to eliminate unnecessary asynchronous calls. Update UUID dependency to v5 for better ID generation. Clean up .gitignore by removing .env entry and adjust Qdrant database methods for improved collection management.

---
+++ teste.ts
@@ -0,0 +1,4 @@
+const GEMINI_API_KEY = 'AIzaSyD-jxW9m0TN023123123dlNK0zXu58Wbsd8L0yW23k'
+
+const QDRANT_URL = 'https://a63cb15f-27c2-4eff-8as240-e8ee306be524.us-east-1-1.a2ws.cloud.qdasdadasdrant.io:6333'
+const QDRANT_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6Ik123123pXVCJ9.eyJhY2Nlc3MiOiJtIn0.Ya123123mSgaRleKmFTcp1HxZO5VCIODS9--wg_9fYEIbweM0'
